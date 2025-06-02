58868dcb026e42ce70b83e612d67c26f47770719
sahincoder <sahincoderbd@gmail.com>
2025-06-02 06:28:34 +0600

    somthing

--- src/components/country/country.jsx
+++ src/components/country/country.jsx
@@ -1,6 +1,11 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './country.css'
 const Country = ({country}) => {
+ const [visited,setVisited]=useState(false);
+
+ const visitedHandler=()=>{
+  setVisited(!visited);
+ }
 
 console.log(country);
 const {name,flags,maps,capital,coatOfArms,flag,population}=country;
@@ -12,6 +17,7 @@ const {name,flags,maps,capital,coatOfArms,flag,population}=country;
             <p><strong>Capital: </strong>{capital} </p>
             <p><strong>Population: </strong>{population} </p>
             <a href={maps.googleMaps} target='blank' >View in Map</a>
+            <button onClick={visitedHandler} >{visited?'Visited':'Going'}</button>
 
         </div>
     );
