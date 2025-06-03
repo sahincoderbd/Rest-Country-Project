74f30ae5a11d473926d97497841013b6b88cfb9f
sahincoder <sahincoderbd@gmail.com>
2025-06-03 07:18:40 +0600

    visited and non visited interaction added , in first click country marked as visited and in second click it remove from visited array using map and filter mathod

--- src/components/countries/countries.jsx
+++ src/components/countries/countries.jsx
@@ -7,7 +7,11 @@ import "./countries.css"
 export default function Countries(){
 
   const [countries,setCountries]=useState([]);
+
   const [visitedCountry,setVisitedCountry]=useState([]);
+
+  const [visitedCountryFlags,setVisitedCountryFlags]=useState([]);
+
 
    useEffect(()=>{
    fetch('https://restcountries.com/v3.1/all')
@@ -18,18 +22,42 @@ export default function Countries(){
    },[])
 
    const handleVisitedCountry=country=>{
-    console.log(country);
-    const newVisitedCountries=[...visitedCountry,country];
-    setVisitedCountry(newVisitedCountries);
+    if(!visitedCountry.includes(country)){
+      const newVisitedCountries=[...visitedCountry,country];
+      setVisitedCountry(newVisitedCountries);
+    }
+
+   }
+
+   const removeVisitedCountry=toremovevisitedcountry=>{
+    const newVisitedArray=visitedCountry.filter( c=> c !== toremovevisitedcountry);
+    setVisitedCountry(newVisitedArray);
+    const newVisitedFlags= visitedCountryFlags.filter(f=> f !== toremovevisitedcountry.flags.svg)
+   setVisitedCountryFlags(newVisitedFlags);
+   }
+
+   const handleVisitedCountryFlags=flag=>{
+   console.log(flag);
+   if (!visitedCountryFlags.includes(flag)){
+    const newVisitedCountriesFlagsArray=[...visitedCountryFlags,flag];
+    setVisitedCountryFlags(newVisitedCountriesFlagsArray);
+    }
+   
    }
     return (
 
         <div>
         <h3>Countries : {countries.length}</h3>
         <h3>Visited Countries : {visitedCountry.length}</h3>
-       
-        <ul>
+        <div className="visited-flags">
+        {
+            visitedCountryFlags.map((flag,id)=><img key={id} src={flag}></img>)
+          }
 
+        </div>
+        
+        <ul>
+           
           {
             visitedCountry.map(country=><li key={country.ccn3}> {country.name.common}</li>)
           }
@@ -37,7 +65,7 @@ export default function Countries(){
           <div className="countries">
 
           {
-            countries.map(country=><Country handleVisitedCountry={handleVisitedCountry} key={country.cca3} country={country}></Country>
+            countries.map(country=><Country removeVisitedCountry={removeVisitedCountry} handleVisitedCountryFlags={handleVisitedCountryFlags} handleVisitedCountry={handleVisitedCountry} key={country.cca3} country={country}></Country>
         )
 
 
--- src/components/country/country.jsx
+++ src/components/country/country.jsx
@@ -1,15 +1,21 @@
 import React, { useState } from 'react';
 import './country.css'
-const Country = ({country,handleVisitedCountry}) => {
+const Country = ({country,handleVisitedCountry,handleVisitedCountryFlags,removeVisitedCountry}) => {
+
+const {name,flags,maps,capital,coatOfArms,flag,population}=country;
+
  const [visited,setVisited]=useState(false);
 
  const visitedHandler=()=>{
   setVisited(!visited);
   handleVisitedCountry(country);
+  handleVisitedCountryFlags(flags.svg);
+  if(visited){
+    removeVisitedCountry(country);
+  }
  }
 
 // console.log(country);
-const {name,flags,maps,capital,coatOfArms,flag,population}=country;
     return (
         <div className={`country ${visited && 'visited'}`}>
             <img  src={flags.png} alt="" />
@@ -18,7 +24,7 @@ const {name,flags,maps,capital,coatOfArms,flag,population}=country;
             <p><strong>Capital: </strong>{capital} </p>
             <p><strong>Population: </strong>{population} </p>
             <a href={maps.googleMaps} target='blank' >View in Map</a>
-            <button onClick={visitedHandler} >{visited?'Visited':'Mark As Visited'}</button>
+            <button onClick={visitedHandler} >{visited? 'Visited':'Mark as visited'}</button>
 
         </div>
     );
